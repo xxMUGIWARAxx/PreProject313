b0e1c0801a8454c235a030f10e683b6fdc4f4b88
xxMUGIWARAxx <misha_grigoryan_83@mail.ru>
2025-07-21 23:18:38 +0300

    commit number 3

--- src/main/java/web/PreProject311/service/UserServiceImpl.java
+++ src/main/java/web/PreProject311/service/UserServiceImpl.java
@@ -4,47 +4,50 @@ package web.PreProject311.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import web.PreProject311.dao.UserDao;
 import web.PreProject311.model.User;
+import web.PreProject311.repositories.UserRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
-    private final UserDao userDAO;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserServiceImpl(UserDao userDAO) {
-        this.userDAO = userDAO;
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<User> index() {
-        return userDAO.index();
+        return userRepository.findAll();
     }
 
     @Override
     @Transactional(readOnly = true)
     public User show(int id) {
-        return userDAO.show(id);
+        Optional<User> foundPerson = userRepository.findById(id);
+        return foundPerson.orElse(null);
     }
 
     @Override
     @Transactional
     public void save(User user) {
-        userDAO.save(user);
+        userRepository.save(user);
     }
 
     @Override
     @Transactional
     public void update(int id, User updatedUser) {
-        userDAO.update(id, updatedUser);
+        updatedUser.setId(id);
+        userRepository.save(updatedUser);
     }
 
     @Override
     @Transactional
     public void delete(int id) {
-        userDAO.delete(id);
+        userRepository.deleteById(id);
     }
 }
